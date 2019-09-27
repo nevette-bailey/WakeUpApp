82b6057adead7a947eb265ce74abad54e7e7d9f2
Nevette Bailey <nevettebailey@gmail.com>
2019-09-27 12:31:37 -0400

    Feature: weather api route

--- server/api/weather.js
+++ server/api/weather.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
-const { Weather } = require('../db/models');
+const { Weather, Shoe } = require('../db/models');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 router.get('/:temp', async (req, res, next) => {
   try {
     const temp = Number(req.params.temp);
-    const weatherByTemp = await Weather.
+    const weatherByTemp = await Weather.findAll({
+      where: {
+        [Op.contains]: temp
+      },
+      include: [{ model: Shoe }]
+    });
+    res.json(weatherByTemp);
   } catch (error) {
     next(error);
   }
