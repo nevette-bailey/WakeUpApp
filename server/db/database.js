33a92b1706d21068d7d6bc3e2cd691423914a488
Nevette Bailey <nevettebailey@gmail.com>
2019-11-06 16:34:34 -0500

    Fix: db hosted on heroku

--- server/db/database.js
+++ server/db/database.js
@@ -12,12 +12,12 @@ console.log(chalk.yellow('Opening database connection'));
 // };
 
 const connectionString = process.env.DATABASE_URL;
+// console.log(process.env.DATABASE_URL, 'DATABASE_URL');
 
-
-// // this doesn't work - try to figure it out!
-// const db = new Sequelize(`${connectionString}`, {
+// // this doesn't work - try to figure out how to connect without static credentials
+// const db = new Sequelize(process.env.DATABASE_URL, {
 //   logging: false,
-//   protocol: null,
+//   protocol: 'postgres',
 //   dialect: 'postgres',
 //   dialectOptions: {
 //     ssl: true
@@ -25,26 +25,25 @@ const connectionString = process.env.DATABASE_URL;
 // });
 
 //this connects to local database!
-const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
-  logging: false // so we don't see all the SQL queries getting made
-});
-
+// const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
+//   logging: false // so we don't see all the SQL queries getting made
+// });
 
 // // this works - connects to Heroku database!
-// const db = new Sequelize(
-//   'd9g93tlmh82l4f',
-//   'ifydcsqocmcycq',
-//   '8945fee9f8310c70f576a4811b284fce8f4649a7f94defbdab813172c95f4728',
-//   {
-//     host: 'ec2-23-21-156-171.compute-1.amazonaws.com',
-//     port: 5432,
-//     logging: false,
-//     protocol: null,
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: true
-//     }
-//   }
-// );
+const db = new Sequelize(
+  'd9g93tlmh82l4f',
+  'ifydcsqocmcycq',
+  '8945fee9f8310c70f576a4811b284fce8f4649a7f94defbdab813172c95f4728',
+  {
+    host: 'ec2-23-21-156-171.compute-1.amazonaws.com',
+    port: 5432,
+    logging: false,
+    protocol: null,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  }
+);
 
 module.exports = db;
